436e4efe43891b28a83951d66b5946192ebdea02
Patrick Dantas <patrick.dantas@inventsoftware.com.br>
2018-02-20 17:23:50 -0300

    adição dos comandos image, wolfram

--- botrick.py
+++ botrick.py
@@ -2,71 +2,22 @@
 
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler)
 from random import randint
+from bs4 import BeautifulSoup
 import logging
 import urllib.request
 import urllib.parse
 import re
 import simplejson
-import io
-
-from bs4 import BeautifulSoup
 import requests
 import re
-import urllib2
-import os
-import cookielib
 import json
+import math
+import wolframalpha
+
+wolframclient = wolframalpha.Client('V6HL75-H6WJWKPYEQ')
 
 def get_soup(url,header):
-    return BeautifulSoup(urllib2.urlopen(urllib2.Request(url,headers=header)),'html.parser')
-
-
-query = raw_input("query image")# you can change the query for the image  here
-image_type="ActiOn"
-query= query.split()
-query='+'.join(query)
-url="https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch"
-print url
-#add the directory for your image here
-DIR="Pictures"
-header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
-}
-soup = get_soup(url,header)
-
-
-ActualImages=[]# contains the link for Large original images, type of  image
-for a in soup.find_all("div",{"class":"rg_meta"}):
-    link , Type =json.loads(a.text)["ou"]  ,json.loads(a.text)["ity"]
-    ActualImages.append((link,Type))
-
-print  "there are total" , len(ActualImages),"images"
-
-if not os.path.exists(DIR):
-            os.mkdir(DIR)
-DIR = os.path.join(DIR, query.split()[0])
-
-if not os.path.exists(DIR):
-            os.mkdir(DIR)
-###print images
-for i , (img , Type) in enumerate( ActualImages):
-    try:
-        req = urllib2.Request(img, headers={'User-Agent' : header})
-        raw_img = urllib2.urlopen(req).read()
-
-        cntr = len([i for i in os.listdir(DIR) if image_type in i]) + 1
-        print cntr
-        if len(Type)==0:
-            f = open(os.path.join(DIR , image_type + "_"+ str(cntr)+".jpg"), 'wb')
-        else :
-            f = open(os.path.join(DIR , image_type + "_"+ str(cntr)+"."+Type), 'wb')
-
-
-        f.write(raw_img)
-        f.close()
-    except Exception as e:
-        print "could not load : "+img
-        print e
-
+    return BeautifulSoup(urllib.request.urlopen(urllib.request.Request(url,headers=header)),'html.parser')
 
 starts = {
 		1:"que foi desgraça",
@@ -103,15 +54,18 @@ def videosearch(search):
 	search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
 	return("http://www.youtube.com/watch?v=" + search_results[0])
 def imagesearch(search):
-    fetcher = urllib.request.build_opener()
-    startIndex = "0"
-    searchUrl = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + search + "&start=" + startIndex
-    print(searchUrl)
-    f = fetcher.open(searchUrl)
-    deserialized_output = simplejson.load(f)
-    imageUrl = deserialized_output['responseData']['results'][0]['unescapedUrl']
-    file = io.StringIO(urllib.request.urlopen(imageUrl).read())
-    return(file)
+    search= search.split()
+    search='+'.join(search)
+    url="https://www.google.co.in/search?q="+search+"&source=lnms&tbm=isch"
+    header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+    soup = get_soup(url,header) 
+    ActualImage=[]# contains the link for Large original image, type of image
+    for a in soup.find_all("div",{"class":"rg_meta"}):
+        link , Type =json.loads(a.text)["ou"]  ,json.loads(a.text)["ity"]
+        ActualImage.append((link,Type))
+        return(link)
+def wolframexpression(expression):
+	return wolframclient.query(expression)
 
 updater = Updater(token='547982491:AAH9dUGZatOuFHiOsI9fg1rU1oSIJHxP-cw')
 dispatcher = updater.dispatcher
@@ -126,13 +80,6 @@ def start(bot, update):
 start_handler = CommandHandler('start',start)
 dispatcher.add_handler(start_handler)
 
-def die(bot, update): 
-	bot.send_message(chat_id=update.message.chat_id, text= "finalmente, adeus")
-	updater.stop()
-
-die_handler = CommandHandler('die',die)
-dispatcher.add_handler(die_handler)
-
 def kill(bot, update): 	
 	bot.send_message(chat_id=update.message.chat_id, text= "infelizmente não sei como faz pra matar alguém")
 	bot.send_message(chat_id=update.message.chat_id, text= "também não consigo nem me matar")
@@ -141,28 +88,45 @@ def kill(bot, update):
 kill_handler = CommandHandler('kill',kill)
 dispatcher.add_handler(kill_handler)
 
+def help(bot, update): 
+	bot.send_message(chat_id=update.message.chat_id, text="/kill - matar")
+	bot.send_message(chat_id=update.message.chat_id, text="/image - pesquisar imagem")
+	bot.send_message(chat_id=update.message.chat_id, text="/video- pesquisar vídeo")
+	bot.send_message(chat_id=update.message.chat_id, text="/wolfram - pesquisar qualquer coisa no wolfram")
+
+help_handler = CommandHandler('help', help)
+dispatcher.add_handler(help_handler)
+
 def video(bot, update): 
-	bot.send_message(chat_id=update.message.chat_id, text=videosearch(update.message.text))
-	print(Filters.text)
+	bot.send_message(chat_id=update.message.chat_id, text=videosearch((update.message.text).replace('/video ','')))
 
 video_handler = CommandHandler('video', video)
 dispatcher.add_handler(video_handler)
 
 def image(bot, update): 
-	bot.send_photo(chat_id=update.message.chat_id, file=imagesearch(update.message.text))
-	print(Filters.text)
+	bot.send_photo(chat_id=update.message.chat_id, photo=imagesearch((update.message.text).replace('/image ','')))
 
 image_handler = CommandHandler('image', image)
 dispatcher.add_handler(image_handler)
 
-def echo(bot,update): bot.send_message(chat_id=update.message.chat_id, text= randomreply())
+def wolfram(bot, update): 
+    bot.send_message(chat_id=update.message.chat_id, text= next((wolframexpression((update.message.text).replace('/wolfram ',''))).results).text)
+
+wolfram_handler = CommandHandler('wolfram', wolfram)
+dispatcher.add_handler(wolfram_handler)
+
+def echo(bot,update): 
+	bot.send_message(chat_id=update.message.chat_id, text= randomreply())
 
 echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(echo_handler)
 
-def unknown(bot, update): bot.send_message(chat_id=update.message.chat_id, text="que porra de comando é esse? vai se foder")
+def unknown(bot, update): 
+	bot.send_message(chat_id=update.message.chat_id, text="que porra de comando é esse? vai se foder")
 
 unknown_handler = MessageHandler(Filters.command, unknown)
 dispatcher.add_handler(unknown_handler)
+
+
 
 updater.start_polling()
